26d0ec26254e9e2054acd5b7b25d459c441a5a83
Makaze <izaya.orihara@gmail.com>
2014-05-19 23:03:07 -0400

    Fixed errors.

--- SF_Mafia_Disable_Post_Editing.user.js
+++ SF_Mafia_Disable_Post_Editing.user.js
@@ -12,16 +12,16 @@ edits,
 breadcrumbs,
 i = 0;
 
-if (document.body.id === 'ipboard_body' && document.getElementsByClass('breadcrumb')[0] != null) {
-	for (i = 0, breadcrumbs = document.getElementsByClass('breadcrumb')[0].getElementsByTagName('span'); i < breadcrumbs.lengh; i++) {
+if (document.body.id === 'ipboard_body' && document.getElementsByClassName('breadcrumb')[0] != null) {
+	for (i = 0, breadcrumbs = document.getElementsByClassName('breadcrumb')[0].getElementsByTagName('span'); i < breadcrumbs.length; i++) {
 		if (breadcrumbs[i].textContent.trim() === 'Mafia') {
 			mafia_flag = true;
 		}
 	}
 
 	if (mafia_flag) {
-		if (document.getElementsByClass('post_edit')[0] != null) {
-			for (i = 0, edits = document.getElementsByClass('post_edit'); i < edits.length; i++) {
+		if (document.getElementsByClassName('post_edit')[0] != null) {
+			for (i = 0, edits = document.getElementsByClassName('post_edit'); i < edits.length; i++) {
 				edits[i].style.display = 'none';
 			}
 		}
