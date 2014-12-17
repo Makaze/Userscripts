def60b72565c8b0d540cb3742ee28ab0d3998081
Makaze <izaya.orihara@gmail.com>
2014-12-17 17:03:13 -0500

    Made it work.

--- IP.Board/IP.Board_Forum_Cleanup.user.js
+++ IP.Board/IP.Board_Forum_Cleanup.user.js
@@ -4,26 +4,37 @@
 // @description	Cleans up the IP.Board interface.
 // @include	*
 // @grant	none
-// @version	1.0
+// @version	2.0.0
 // ==/UserScript==
 
 function clean() {
-	var context,
-	title,
+	var commentWalker,
 	collection,
 	i = 0;
 
-	if (document.getElementById('topic_stats') != null) {
-		context = document.getElementById('topic_stats').previousElementSibling;
-		title = context.getElementsByClassName('maintitle')[0];
-		
-		if (title.textContent.trim().substr(0, 11) === "Also tagged") {
-			context.style.display = 'none';
+	if (document.getElementsByClassName('ipsTag')[0] != null && document.getElementsByClassName('topic_list')[0] != null) {
+		commentWalker = document.createTreeWalker(document, NodeFilter.SHOW_COMMENT, { 
+			acceptNode: function() {
+				return NodeFilter.FILTER_ACCEPT;
+			}
+		}, false);
+
+		while(commentWalker.nextNode()) {
+			if (commentWalker.currentNode.nodeValue.indexOf('SAME TAGGED') > -1) {
+				break;
+			}
+		}
+
+		if (commentWalker.currentNode.nextElementSibling && commentWalker.currentNode.nextElementSibling.nextElementSibling) {
+			commentWalker.currentNode.nextElementSibling.style.display = 'none';
+			commentWalker.currentNode.nextElementSibling.nextElementSibling.style.display = 'none';
+			console.log('Cleaned up', commentWalker.currentNode.nextElementSibling, commentWalker.currentNode.nextElementSibling.nextElementSibling);
 		}
 	}
 
 	for (i = 0, collection = document.getElementsByClassName('shareButtons'); i < collection.length; i++) {
 		collection[i].style.display = 'none';
+		console.log('Cleaned up', collection[i]);
 	}
 }
 
