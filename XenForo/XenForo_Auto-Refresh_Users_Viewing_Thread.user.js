c0e683e19121dbed1fad37e51bd22eecf041fce1
Makaze <izaya.orihara@gmail.com>
2014-12-17 21:30:04 -0500

    Initial commit

---
+++ XenForo/XenForo_Auto-Refresh_Users_Viewing_Thread.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name	XenForo - Auto-refresh Users Viewing Thread 
+// @namespace	Makaze
+// @description	Refreshes the 'Users Viewing Thread' field every 10 seconds.
+// @include	*kh-vids.net/*
+// @grant	none
+// @version	1.0.0
+// ==/UserScript==
+
+var thread;
+
+function runInGlobal(code) {
+	var scripts = document.createElement('script');
+	scripts.type = 'text/javascript';
+	scripts.id = 'runInGlobal';
+	scripts.appendChild(document.createTextNode(
+		code + '\n\n' +
+		'document.getElementById(\'runInGlobal\').remove();'
+	));
+
+	(document.head || document.body || document.documentElement).appendChild(scripts);
+}
+
+function main(thread) {
+	document.addEventListener('keydown', function(event) {
+		if (event.keyCode === 116 && event.shiftKey) {
+			event.preventDefault();
+			$('.online').load('/threads/' + thread + '/ .online > *', function() {
+				$(this).children().fadeTo(50, 0.2).fadeTo(1500, 1, 'easeInOutQuad');
+			});
+		}
+	}, false);
+
+	setInterval(function() {
+		var currentstate = document.getElementsByClassName('online')[0].innerHTML;
+		$('.online').load('/threads/' + thread + '/ .online > *', function() {
+			$(this).children().fadeTo(50, 0.2).fadeTo(1500, 1, 'easeInOutQuad');
+			if (currentstate !== document.getElementsByClassName('online')[0].innerHTML) {
+				console.log($('.online .footnote').text(), '::', $('.online .listInline').text().replace(/\\s+/g, ' ').trim());
+			}
+		});
+	}, 10000);
+}
+
+if (document.documentElement.id === 'XenForo') {
+	if (window.location.pathname.substr(1, 7) === 'threads' && document.getElementsByClassName('online')[0] != null) {
+		thread = window.location.pathname.match(/\/threads\/.*\.(\d+)/)[1];
+
+		runInGlobal(
+			main.toString() +
+			"main('" + thread + "');"
+		);
+	}
+}
